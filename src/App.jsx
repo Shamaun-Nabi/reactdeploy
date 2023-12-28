44df7c5630b5f9346e9abc18d044d9952b69d655
Shamaun-Nabi@ <dev.shamaun@gmail.com>
2023-12-28 20:10:33 +0600

    CHANGES

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-center text-3xl">Hello React</h1>
+      <h1 className="text-center text-3xl">
+        React After Deploy With Github and FTP
+      </h1>
     </>
   );
 }
